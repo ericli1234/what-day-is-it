0dd6eefec95863c1bdd7310edd7e4e84fbc06302
ericli1234 <li.eric00@gmail.com>
2015-04-05 14:58:43 -0400

    Added Formatting
    Fonts, Separated Date from Time

--- index.php
+++ index.php
@@ -4,11 +4,14 @@
 		<meta charset="utf-8">
 		<title>What Day Is It?</title>
 		<link rel="stylesheet" href="css/style.css" />
+		<link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Open%20Sans"/>
 	</head>
 	<body>
-		<?php 
-		echo '<span id="date">' . date("F j, Y, g:i A T") . '</span>'; 
-		echo '<span id="day">' . Day X . '</span>'; 
+		<?php
+		$date = 1;
+		echo '<h1 id="date">' . date("F j Y") . '</h1>'; 
+		echo '<h1 id="time">' . date("g:i A T") . '</h1>'; 
+		echo '<h1 id="day">' . 'Day ' . $date . '</h1>'; 
 		?> 
 	</body>
 </html>
