0bb6b2fe1007601456990644656d7f9570d64184
ericli1234 <li.eric00@gmail.com>
2015-04-05 11:43:33 -0400

    Created Index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>What Day Is It?</title>
+		<link rel="stylesheet" href="css/style.css" />
+	</head>
+	<body>
+		<?php 
+		?> 
+	</body>
+</html>
